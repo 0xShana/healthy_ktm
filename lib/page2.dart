8febaf82b7f5f619d188cac8bb8f0c06809cf51b
shana james <shanajames@shanas-MacBook-Air.local>
2020-03-18 14:12:19 +0530

    Initial commit

---
+++ lib/page2.dart
@@ -0,0 +1,104 @@
+import 'package:day12_login/Animation/FadeAnimation.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    )
+);
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.white,
+        body: SingleChildScrollView(
+          child: Container(
+            child: Column(
+              children: <Widget>[
+                Container(
+                  height: 400,
+                  decoration: BoxDecoration(
+                      image: DecorationImage(
+                          image: AssetImage('assets/images/img1.jpg'),
+                          fit: BoxFit.fitWidth
+                      )
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(30.0),
+                  child: Column(
+                    children: <Widget>[
+                      FadeAnimation(1.5, Text("WELCOME TO HEALTHY KOTTAYAM !", style: TextStyle(color: Colors.black, fontSize: 40, fontWeight: FontWeight.bold),)),
+                      FadeAnimation(1.8, Container(
+                        padding: EdgeInsets.all(5),
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(10),
+                            boxShadow: [
+                              BoxShadow(
+                                  color: Color.fromRGBO(143, 148, 251, .2),
+                                  blurRadius: 20.0,
+                                  offset: Offset(0, 10)
+                              )
+                            ]
+                        ),
+                        child: Column(
+                          children: <Widget>[
+                            /*Container(
+														padding: EdgeInsets.all(8.0),
+														child: TextField(
+															decoration: InputDecoration(    // welcome text
+																	border: InputBorder.none,
+																	hintText: "Password",
+																	hintStyle: TextStyle(color: Colors.grey[400])
+															),
+														),
+													),*/
+
+
+                            Container(
+                              padding: EdgeInsets.all(8.0),
+                              decoration: BoxDecoration(
+                                  border: Border(bottom: BorderSide(color: Colors.grey[100]))
+                              ),
+                              child: TextField(
+                                decoration: InputDecoration(
+                                    border: InputBorder.none,
+                                    hintText: "Enter OTP ",
+                                    hintStyle: TextStyle(color: Colors.grey[400])
+                                ),
+                              ),
+                            ),
+
+                          ],
+                        ),
+                      )),
+                      SizedBox(height: 30,),
+                      FadeAnimation(2, Container(
+                        height: 50,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            gradient: LinearGradient(
+                                begin: Alignment.topRight,
+                                end: Alignment.bottomLeft,
+                                colors: [Colors.white, Colors.blue])
+                        ),
+                        child: Center(
+                          child: Text("VERIFY OTP", style: TextStyle(color: Colors.white,fontSize: 30, fontWeight: FontWeight.bold),),
+                        ),
+                      )),
+                      SizedBox(height: 70,),
+
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+        )
+    );
+  }
+}
+
